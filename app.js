ea3c5d5a943329c0fb32070c1937c3a0f03cae8c
Elton Goncalves <eltongonc@gmail.com>
2017-04-08 22:13:50 +0200

    .

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use(function(err, req, res, next) {
 
 app.set('port', process.env.PORT || 3000);
 
-app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function() {
     console.log(server.address());
   console.log('Express server listening on port ' + server.address().port);
 });
