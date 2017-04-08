16cb37fc8eb739571075455a8cd93979789bd1df
Elton Goncalves <eltongonc@gmail.com>
2017-04-08 18:49:10 +0200

    .

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(8080, 'APP_PRIVATE_IP_ADDRESS');
-console.log('Server running at http://APP_PRIVATE_IP_ADDRESS:8080/');
+}).listen(8080, '10.129.17.123');
+console.log('Server running at http://10.129.17.123:8080/');
